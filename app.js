a0631fa85cf4b0e9e9d3e0e21859e2b3589219ff
heisam alali <heisam.alali@outlook.com>
2023-09-24 23:01:35 +0330

    add constructor function

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello')
+function Person(){
+    this.firstname = "Default"
+    this.lastName = "Default"
+}
+
+Person.prototype.GetFullName = function(){
+    return this.firstname + ' '+ this.lastName;
+}
+
+var heisam = new Person();
+heisam.firstname = 'Heisam';
+heisam.lastName = 'Aleali';
+
+console.log(heisam.GetFullName())
