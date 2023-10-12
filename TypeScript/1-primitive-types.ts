d27e50163d42797ffdd9964e4adcaff0520d900b
heisam alali <heisam.alali@outlook.com>
2023-10-13 01:32:38 +0330

    add typescripts

---
+++ TypeScript/1-primitive-types.ts
@@ -0,0 +1,8 @@
+let myNumber: "b" | "a";
+myNumber = "a";
+let unit: number | string;
+//unit = true this will not work
+
+// to start quokka => Ctrl + shift + p
+let firstName: string = "heisam";
+console.log(firstName + "alali");
